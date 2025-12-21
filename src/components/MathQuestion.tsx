c57b53b5ea5a9335da191e996bb70a36614a61bf
Chi Shang Cheng <info@humanrobotrobot.com>
2025-12-21 22:11:43 +0100

    Visually center equation in MathQuestion component

--- src/components/MathQuestion.tsx
+++ src/components/MathQuestion.tsx
@@ -1,4 +1,5 @@
 import MathQuestionType from "../quiz/MathQuestion";
+import styles from "../styles/MathQuestion.module.css";
 import type { QuestionProps } from "./Question.tsx";
 
 interface MathQuestionProps extends QuestionProps {
@@ -13,14 +14,18 @@ export default function MathQuestion({
   setAnswer,
 }: MathQuestionProps) {
   return (
-    <>
-      <p>{`${question.a} ${question.operation} ${question.b} = ?`}</p>
-      <input
-        type="text"
-        value={answer}
-        onChange={(event) => setAnswer(event.target.value)}
-      />
+    <section className={styles.mathQuestion}>
+      <p className={styles.equation}>
+        {question.a} {question.operation} {question.b} ={" "}
+        <input
+          className={styles.numberInput}
+          type="text"
+          value={answer}
+          onChange={(event) => setAnswer(event.target.value)}
+        />
+      </p>
+
       {isAnswered && <p>Het antwoord is {isCorrect ? "juist" : "onjuist"}</p>}
-    </>
+    </section>
   );
 }
